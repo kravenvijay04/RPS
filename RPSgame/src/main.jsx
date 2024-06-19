c02a79bdc1518d0a00d44e83ed75315c029f2fbb
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-06-19 12:13:38 +0530

    main jsx file is added

---
+++ RPSgame/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Game from './Game.jsx'
+import './game.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+)
